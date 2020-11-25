69d5bd6cff94e7c126ee78351e0eb39ffde81abb
Sam Toll <sam.x.toll@gmail.com>
2020-11-24 21:48:31 -0800

    func5 - Create An Account

--- src/Pages/CreateAccount.js
+++ src/Pages/CreateAccount.js
@@ -5,9 +5,9 @@ import "bootstrap/dist/css/bootstrap.css";
 import UserContext from '../store/context';
 
 const CreateAccount = () => {
-    // const [value, setValue] = useState("Profile");
-    // const {state, actions} = useContext(Context);
     var title = "Login: ";
+
+    // user can create an account successfully
 
     const context = useContext(UserContext);
 
